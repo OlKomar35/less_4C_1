f89550684adb389120fc72431a140d04fd892626
Olga Komar <komar_ol@mail.ru>
2023-04-24 10:58:12 +0300

    added the solution to the first task

--- Program.cs
+++ Program.cs
@@ -3,5 +3,22 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+int NumberPrint(string msg){
+    Console.Write($"Введите {msg} =");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
 
+int Degree(int num, int deg){
+    int res = 1;
+    for (int i=1; i<= deg; i++){
+        res=res*num;
+    }
+    return res;
+}
+
+
+int a = NumberPrint("A");
+int b = NumberPrint("B");
+Console.WriteLine($"{a} в степени {b} = {Degree(a,b)}");
 
